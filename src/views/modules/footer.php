4c8281d25b1aff9deafbf39309f9e396d9919cae
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-11-09 18:02:50 +0100

    Footer - Structure

--- src/views/modules/footer.php
+++ src/views/modules/footer.php
@@ -1,3 +1,34 @@
+<footer>
+    <div class="container">
+        <div class="main-footer">
+            <div class="row">
+                <div class="col-12 col-sm-3">
+                    <img src="<?= frontFolder('/img/logo.png') ?>" alt="" class="img-fluid">
+                </div>
+                <div class="col-12 col-sm-4">
+                    <h4>Nous contacter</h4>
+                    <a href="#" class="btn btn-secondary">contact@annonces-auto.fr</a>
+                </div>
+                <div class="col-12 col-sm-5">
+                    <h4>Annonces auto</h4>
+                    <ul>
+                        <li><a href="#">A propos du site annonces-auto.fr</a></li>
+                        <li><a href="#">Se connecter / S'inscrire</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="sub-footer">
+            <ul>
+                <li><a href="#">Politiques de confidentialités</a></li>
+                <li><a href="#">Plan du site</a></li>
+                <li>Réalisation : <a href="#">Sébastien Gaudard</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
 <script src="<?= frontFolder('/lib/jquery/jquery-3.4.1.min.js') ?>"></script>
 <script src="<?= frontFolder('/lib/slick/slick.min.js') ?>"></script>
 <script src="<?= frontFolder('/js/slicks.js') ?>"></script>
