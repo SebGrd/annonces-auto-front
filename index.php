e43b6e6d6cb20cf92c28587df2f440b6667ed185
Sebastien Gaudard <sebastien.gaudard.1@gmail.com>
2019-11-19 16:21:28 +0100

    Création du routeur

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
-<?php require 'src/function.php'?>
-<?php require 'src/views/modules/header.php'?>
-<?php require 'src/views/home.php'?>
-<?php require 'src/views/modules/footer.php'?>
+<?php
+$url = $_GET['url'];
+function buildPage($controller){
+    require 'src/function.php';
+    require 'src/views/modules/header.php';
+    require $controller;
+    require 'src/views/modules/footer.php';
+};
+
+
+if (empty($url)){
+    buildPage('src/views/home.php');
+} else{
+    switch ($url) {
+        case "liste-annonces":
+            buildPage('src/views/liste-annonces.php');
+            break;
+        default:
+            buildPage('src/views/404.php');
+            break;
+    }
+}
+
+?>
+
+
